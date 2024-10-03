05750fc04e02707f495228d253460428472e2100
jjjlyk <jelykamae05@gmail.com>
2024-10-03 23:18:00 +0800

    fixed errors

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import streamlit as st
-import matplotlib as plt 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import seaborn as sns 
+import seaborn as sns
 import altair as alt
-from wordcloud import WordCloud
 from mpl_toolkits.mplot3d import Axes3D
+import networkx as nx
+import plotly.graph_objects as go
+from io import StringIO
 
 st.title("Customer Purchase Behavior")
 st.header("Electronic Sales Data")
 st.write("link to the data set: [](https://www.kaggle.com/datasets/cameronseamons/electronic-sales-sep2023-sep2024?fbclid=IwZXh0bgNhZW0CMTEAAR1m9hzGHXXRPAO7gVhMfL-rGpGiEJWvKSazkSzZk1lNeZqCp2LlhLLZfZQ_aem_d7rG1iddZ9T4F96wYst2FQ)")
+st.markdown('`by Group 1`')
 
 # Read our CSV dataset.
 df = pd.read_csv("Electronic_sales.csv")
