0fdda020e193383dcfbb9f01dbe1e53b15cc0944
jjjlyk <jelykamae05@gmail.com>
2024-10-03 23:00:28 +0800

    added csv

--- main.py
+++ main.py
@@ -10,3 +10,8 @@ from mpl_toolkits.mplot3d import Axes3D
 st.title("Customer Purchase Behavior")
 st.header("Electronic Sales Data")
 st.write("link to the data set: [](https://www.kaggle.com/datasets/cameronseamons/electronic-sales-sep2023-sep2024?fbclid=IwZXh0bgNhZW0CMTEAAR1m9hzGHXXRPAO7gVhMfL-rGpGiEJWvKSazkSzZk1lNeZqCp2LlhLLZfZQ_aem_d7rG1iddZ9T4F96wYst2FQ)")
+
+# Read our CSV dataset.
+df = pd.read_csv("Electronic_sales.csv")
+
+df
